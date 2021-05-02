0c826a45d614c9411e8b9308db74b635db1a8de5
Henrique Tavares <ihenrits@gmail.com>
2021-05-02 16:30:24 -0300

    creating project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Hi, Henrique here!  <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="30px">
+
+This is my NPX card to connect with me directly via console or terminal
+
+## Usage
+
+Just hit, and get know me in your terminal 👇
+
+```sh
+npx henrique
+```
+
+This project was based on the [Carine Batista](https://github.com/carinebatista/npx_card#readme) project. Thanks for the inspiration.
---
+++ card.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+
+// Modules
+const boxen = require("boxen");
+const chalk = require("chalk");
+
+const data = {
+    name: chalk.red("                  Henrique Tavares"),
+    work: `${chalk("Front End Developer at")} ${chalk.redBright("Zaal Tecnologia")}`,
+
+    github: chalk.gray("https://github.com/") + chalk.blue("tavareshenrique"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.green("tavareshenrique"),
+    email: chalk.blue("ihenrits@gmail.com"),
+    web: chalk.yellow("https://henriquetavares.com"),
+    skills: chalk.blueBright("JS, TS, React, React Native, Node"),
+
+    npx: chalk.red("npx henrique"),
+
+    labelWork: ("         "),
+    labelGitHub: chalk.white.bold("        GitHub:"),
+    labelLinkedIn: chalk.white.bold("      LinkedIn:"),
+    labelEmail: chalk.white.bold("         Email:"),
+    labelWeb: chalk.white.bold("           Web:"),
+    labelSkills: chalk.blue.bold("        Skills:"),
+    labelCard: chalk.white.bold("          Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelEmail}  ${data.email}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelSkills}  ${data.skills}`,
+        ``,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        
+        `${chalk.italic(
+            "                       think different. "
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "bold",
+        borderColor: "magenta"
+    }
+);
+
+console.log(me);
